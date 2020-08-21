88c0f539d3a140fadd5871115e04f96d28730f2f
Breno <breno.novais@hotmail.com>
2020-08-21 17:20:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './Appantigo';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { view, text } from 'react-native'
+
+export default class main extends Component {
+    render () {
+        return (
+            <View>
+                <Text>Página Main</Text>
+            </View>
+        );
+    }
+}
